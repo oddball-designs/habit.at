b33746ca9f52018fc4a6dffa87ba759e1f8bd189
Philip Reitz <philipreitz@Philips-MacBook-Air.local>
2016-07-05 16:57:22 -0600

    create users

--- routes/index.js
+++ routes/index.js
@@ -18,18 +18,19 @@ router.post('/', function(req, res, next){
   console.log(req.body);
   knex('users').where({email: req.body.email}).then(function(data){
     console.log(Number(req.body.password));
-    console.log(data[0].password);
-    bcrypt.compare(Number(req.body.password), data[0].password, function(err, result){
+    console.log(data[0]);
+    bcrypt.compare(req.body.password, data[0].password, function(err, result){
       if(err){
         console.log("help");
       }
       //next(err);
       else{
+        console.log(result);
         if(result){
           req.session.id = data[0].id;
           req.session.first_name = data[0].first_name;
           req.session.isAdmin = data[0].is_admin;
-          res.redirect('/households/' + data[0].household_id + '/users/' + data[0].user_id);
+          res.redirect('/households/' + data[0].household_id + '/users/' + data[0].id);
         }
         else{
           res.send("Your email or password was invalid.");
