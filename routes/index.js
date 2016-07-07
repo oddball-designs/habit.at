99fecab1c5537a3aade632bfa324c0293f910619
Stephanie Ogburn <stephanie.ogburn@gmail.com>
2016-07-07 15:05:04 -0600

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -53,6 +53,7 @@ router.get('/logout', function(req, res, next){
   res.redirect('/');
 });
 
+// runs on chron job on heroku 
 router.get('/trigger', function(req, res, next){
   knex.select('users.first_name', 'users.phone_number', 'tasks.title', 'tasks.description', 'due_date').from('tasks').leftJoin('users', 'tasks.user_id', 'users.id').where('due_date', new Date().toISOString().slice(0,10))
   .then(function(data){
@@ -79,7 +80,7 @@ function sendMessage(ph,title){
     to: '+1' + ph, // Any number Twilio can deliver to
     from: '+19705333115', // A number you bought from Twilio and can use for outbound communication
     body: 'your task to ' + title + ' is due today. Log on to habit.at to see more details about your tasks for the day' // body of the SMS message
-    }, function(err, responseData) { 
+    }, function(err, responseData) {
     if (!err) {
         resolve();
       } else {
